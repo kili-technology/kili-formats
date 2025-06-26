f753c0fa75a6c2c4c73e6ef511b88f92c038db52
paulruelle <paul.ruelle@kili-technology.com>
2025-06-26 14:00:13 +0200

    feat(LAB-3719): fix multipart polygon GeoJson export

--- src/kili_formats/__init__.py
+++ src/kili_formats/__init__.py
@@ -2,6 +2,7 @@
 This module provides functions to convert Kili data formats to various other formats"""
 
 from .format.coco import convert_from_kili_to_coco_format
+from .format.geojson import convert_from_kili_to_geojson_format
 from .format.llm import (
     convert_from_kili_to_llm_rlhf_format,
     convert_from_kili_to_llm_static_or_dynamic_format,
@@ -13,6 +14,7 @@ from .kili import clean_json_response, convert_to_pixel_coords, format_json_resp
 __all__ = [
     "clean_json_response",
     "convert_from_kili_to_coco_format",
+    "convert_from_kili_to_geojson_format",
     "convert_from_kili_to_llm_rlhf_format",
     "convert_from_kili_to_llm_static_or_dynamic_format",
     "convert_from_kili_to_voc_format",
